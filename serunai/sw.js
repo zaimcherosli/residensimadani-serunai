99f50450984fee9994539b3903a78c33074a0d1e
Zaim Rosli <huzaimrosli@gmail.com>
2026-09-12 18:14:49 +0800

    feat: add PWA support, web app manifest, service worker, and mobile bottom navigation dock

---
+++ serunai/sw.js
@@ -0,0 +1,37 @@
+// Service Worker for Residensi MADANI Serunai PWA
+const CACHE_NAME = 'serunai-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/serunai/',
+  '/serunai/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
